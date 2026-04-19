3af61c11d4ab028218cfe31b59338be0bb0880f9
kevinaclu <kevinaclu@gmail.com>
2026-04-19 10:52:46 -0700

    add og image fix

--- src/config/wedding.ts
+++ src/config/wedding.ts
@@ -153,6 +153,6 @@ export const meta = {
   title: `Nuestra Boda | ${couple.groomName} & ${couple.brideName}`,
   description: `Invitacion de boda de ${couple.groomName} y ${couple.brideName}`,
   ogImage:
-    "https://invitacion-jaun-maricruz-git-main-kevinaclus-projects.vercel.app/img/og-image.jpg",
-  siteUrl: "https://invitacion-juan-maricruz.vercel.app",
+    "https://invitacion-jaun-maricruz-git-main-kevinaclus-projects.vercel.app/img/og-image.png",
+  siteUrl: "https://invitacion-jaun-maricruz.vercel.app",
 };
