fea25caaf62b54657e178a03f9a42528a738b4ae
kevinaclu <kevinaclu@gmail.com>
2026-05-08 22:28:53 -0700

    Change name chenny

--- src/config/wedding.ts
+++ src/config/wedding.ts
@@ -122,7 +122,7 @@ export const locations = {
 // --- PASTORES ---
 export const pastors = {
   label: "Ceremonia oficiada por",
-  names: ["Isaac Flores", "Chenny Rábago"],
+  names: ["Isaac Flores", "Chenny Flores"],
 };
 
 // --- CODIGO DE VESTIMENTA ---
